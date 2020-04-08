3c59d69138a14ae1877bd053f78a4c3185bf258a
Samyak <samyakjainbvs@gmail.com>
2020-04-09 01:24:00 +0530

    *

--- CommonChecklist/settings.py
+++ CommonChecklist/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'CommonChecklist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(os.path.dirname(BASE_DIR), 'templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
