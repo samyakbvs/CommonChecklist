aeaeeab66d7366e4ab2e16bd1d9c60ea7038c9a9
arjunpandey2003 <arjunpandey2003@gmail.com>
2020-05-18 22:33:34 +0530

    seetings

--- CommonChecklist/settings.py
+++ CommonChecklist/settings.py
@@ -76,25 +76,25 @@ WSGI_APPLICATION = 'CommonChecklist.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+# 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'kepler_db',
+        'USER': 'kepler',
+        # 'USER': 'samyakbvs',
+        'PASSWORD': 'kepler69',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'kepler_db',
-#         'USER': 'kepler',
-#         # 'USER': 'samyakbvs',
-#         'PASSWORD': 'kepler69',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
 
 
 # Password validation
